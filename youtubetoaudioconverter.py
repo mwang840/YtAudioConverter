e4645fc4ed0fa961e1bbc5bc4d195024b880f680
Maxwell Wang <maxwang@udel.edu>
2024-05-30 21:44:36 -0400

    mp3 conversion working

--- youtubetoaudioconverter.py
+++ youtubetoaudioconverter.py
@@ -1,9 +1,9 @@
 from pytube import YouTube
 import requests
-def main():
-    ##Grabs user input, which is a string and must be from the youtube site
-    current_url  = str(input())
-    print(current_url)
+from moviepy.editor import *
+import os
+
+def downloadYtClip(current_url: str):
     req = requests.head(current_url, allow_redirects=True).url
     if "youtube.com" in req:
        try:
@@ -11,15 +11,36 @@ def main():
             print("We have found your video")
        except:
             print("Video Failed, please try again!")
+            return None
        currentStream = video_to_download.streams.get_highest_resolution()
        
        try:
-           currentStream.download()
-           print(type(currentStream.download()))
+           downloaded_file_path = currentStream.download()
+           return downloaded_file_path
        except:
            print("Video failed to download")
     else:
         print("Error youtube link is invalid")
+        return None
+    return
+
+def convertToMp3(clip: str, outputPath: str):
+    downloadedClip = VideoFileClip(clip)
+    audioClip  = downloadedClip.audio
+    audioClip.write_audiofile(clip, codec='libmp3lame')
+    audioClip.close()
+    downloadedClip.close()
+    
+
+def main():
+    ##Grabs user input, which is a string and must be from the youtube site
+    current_url  = "https://www.youtube.com/watch?v=b-ys7ZMn6II"
+    current_path = downloadYtClip(current_url)
+    if current_path:
+        mp3_file_path = current_path.replace(".mp4", ".mp3")
+        convertToMp3(current_path, mp3_file_path)
+        print(f"MP3 file saved to {mp3_file_path}")
+
 
 if __name__ in "__main__":
     main()
