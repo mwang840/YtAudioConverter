80ce105e24672ff8cfbaac378d46bfbeab1d147f
Maxwell Wang <maxwang@udel.edu>
2024-06-02 11:50:19 -0400

    ok so it works once the substring "watch" is in the video, aka if visual content exists. Part one done

--- youtubetoaudioconverter.py
+++ youtubetoaudioconverter.py
@@ -2,8 +2,18 @@ from pytube import YouTube
 import requests
 from moviepy.editor import *
 import os
+from tkinter import *
 
 def downloadYtClip(current_url: str):
+    """
+    Takes in the current youtube link you want to download and returns the path where you downloaded the file
+    
+    Args:
+    current_url (str), the youtube link passed in
+
+    Returns:
+        the directory path where the video is downloaded (assuming the download goes successful)
+    """
     req = requests.head(current_url, allow_redirects=True).url
     if "youtube.com" in req:
        try:
@@ -12,10 +22,11 @@ def downloadYtClip(current_url: str):
        except:
             print("Video Failed, please try again!")
             return None 
-       currentStream = video_to_download.streams.get_highest_resolution()
+       currentStream = video_to_download.streams.filter(file_extension='mp4').order_by("abr").desc().first()
        
        try:
            downloaded_file_path = currentStream.download()
+           print(downloaded_file_path)
            return downloaded_file_path
        except:
            print("Video failed to download")
@@ -25,21 +36,30 @@ def downloadYtClip(current_url: str):
     return
 
 def convertToMp3(clip: str, outputPath: str):
-    downloadedClip = VideoFileClip(clip)
-    audioClip  = downloadedClip.audio
-    audioClip.write_audiofile(clip, codec='libmp3lame')
-    audioClip.close()
-    downloadedClip.close()
+    try:
+        downloadedClip = VideoFileClip(clip)
+        audioClip  = downloadedClip.audio
+        audioClip.write_audiofile(outputPath, codec='libmp3lame')
+        audioClip.close()
+        downloadedClip.close()
+    except Exception as error:
+        print("Looks like you got an error", error)
     
 
 def main():
     ##Grabs user input, which is a string and must be from the youtube site
-    current_url  = "https://www.youtube.com/watch?v=b-ys7ZMn6II"
-    current_path = downloadYtClip(current_url)
-    if current_path:
-        mp3_file_path = current_path.replace(".mp4", ".mp3")
-        convertToMp3(current_path, mp3_file_path)
-        print(f"MP3 file saved to {mp3_file_path}")
+    current_url  = str(input("Enter the youtube video you want to convert\n"))
+    downloadedClip = downloadYtClip(current_url)
+    print(downloadedClip)
+   
+    if downloadedClip != None:
+        current_path = str(input("Enter the folder/directory you want to place your downloaded song into\n"))
+        if not os.path.exists(current_path):
+            os.makedirs(current_path)
+
+        dirPath = os.path.dirname(downloadedClip)
+        mp3_file_path = os.path.join(dirPath, os.path.basename(downloadedClip).replace(".mp4", ".mp3"))
+        convertToMp3(downloadedClip, mp3_file_path)
 
 
 if __name__ in "__main__":
